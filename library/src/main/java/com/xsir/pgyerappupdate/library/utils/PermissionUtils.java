f6b1375e1859086428628c6ce835908fdf34f7fe
xinpengfei <542270559@qq.com>
2019-01-09 11:05:31 +0800

    0.8

--- library/src/main/java/com/xsir/pgyerappupdate/library/utils/PermissionUtils.java
+++ library/src/main/java/com/xsir/pgyerappupdate/library/utils/PermissionUtils.java
@@ -11,18 +11,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Created by x-sir on 2019/1/3 :)
  * Function:apply dynamic permissions utils class.
  */
 public class PermissionUtils {
 
+    private String[] mPermissions;
     private static PermissionUtils mInstance;
     private OnPermissionCallback mOnPermissionCallback;
-    private List<String> mPermissions;
 
     private PermissionUtils() {
     }
@@ -34,11 +31,19 @@ public class PermissionUtils {
         return mInstance;
     }
 
+    /**
+     * 请求申请权限
+     *
+     * @param context              上下文
+     * @param permissions          权限数组
+     * @param onPermissionCallback 授权结果的回调接口
+     */
     public void request(Context context, String[] permissions, OnPermissionCallback onPermissionCallback) {
+        // 如果 >= 6.0 才去申请动态权限
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.mPermissions = permissions;
             this.mOnPermissionCallback = onPermissionCallback;
-            mPermissions = Arrays.asList(permissions);
-            if (!isGranted(context, mPermissions)) {
+            if (!isGranted(context, permissions)) {
                 PermissionActivity.actionStart(context);
             } else {
                 this.mOnPermissionCallback.onGranted();
@@ -48,30 +53,37 @@ public class PermissionUtils {
         }
     }
 
-    private static boolean isGranted(Context context, List<String> permissions) {
+    /**
+     * 判断多个权限是否已经全部授权
+     *
+     * @param context     上下文
+     * @param permissions 权限数组
+     * @return 授权结果
+     */
+    private static boolean isGranted(Context context, String[] permissions) {
         for (String permission : permissions) {
-            if (!isGranted(context, permission)) {
+            boolean result = PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, permission);
+            if (!result) {
                 return false;
             }
         }
         return true;
     }
 
-    private static boolean isGranted(Context context, String permission) {
-        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, permission);
-    }
-
+    /**
+     * 定义一个授权结果的接口回调
+     */
     public interface OnPermissionCallback {
-        void onGranted();
+        void onGranted(); // 授权成功
 
-        void onDenied();
+        void onDenied(); // 权限被拒
     }
 
     public static class PermissionActivity extends Activity {
 
         private static final int REQUEST_CODE = 520;
 
-        public static void actionStart(final Context context) {
+        public static void actionStart(Context context) {
             Intent intent = new Intent(context, PermissionActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
@@ -80,14 +92,7 @@ public class PermissionUtils {
         @Override
         protected void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            if (isGranted(this, mInstance.mPermissions)) {
-                if (mInstance.mOnPermissionCallback != null) {
-                    mInstance.mOnPermissionCallback.onGranted();
-                }
-            } else {
-                String[] permissions = (String[]) mInstance.mPermissions.toArray();
-                ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE);
-            }
+            ActivityCompat.requestPermissions(this, mInstance.mPermissions, REQUEST_CODE);
         }
 
         @Override
@@ -101,7 +106,7 @@ public class PermissionUtils {
                     mInstance.mOnPermissionCallback.onDenied();
                 }
             }
-            finish();
+            PermissionActivity.this.finish();
         }
     }
 
