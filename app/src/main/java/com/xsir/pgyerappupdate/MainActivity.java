3a50aa73234d2a28ac968cb401af6e78d6bae67d
Xin Vance <xinpengfei520@gmail.com>
2021-05-13 18:14:01 +0800

    migrated to MavenCentral from Bintray.

--- app/src/main/java/com/xsir/pgyerappupdate/MainActivity.java
+++ app/src/main/java/com/xsir/pgyerappupdate/MainActivity.java
@@ -1,7 +1,7 @@
 package com.xsir.pgyerappupdate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.xsir.pgyerappupdate.library.PgyerApi;
 
